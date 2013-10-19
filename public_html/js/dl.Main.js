b710d2e6de38a0b0278b5fe3a3901c7157637ddf
Armand Besombes <armand.besombes@epitech.net>
2013-10-19 05:40:38 +0200

    update the loopActions and eventMouse

--- public_html/js/dl.Main.js
+++ public_html/js/dl.Main.js
@@ -1,10 +1,10 @@
 (function () {
-    
-    var FRAME_RATE = 60;
+    var HEIGHT = 500;
+    var WIDTH = 500
     
     var Main = function() {
         this.coccu = [];
-        this.link = null;
+        this.link = new dl.characters.Link(HEIGHT / 2, WIDTH / 2);
         this.background = null;
         this.hud = null;
         this.init();
@@ -13,9 +13,22 @@
     dl.Main = Main;
     var _main = Main.prototype;
     
-    _main.init = function () { 
+    _main.init = function () {
+        var that = this;
+        dl.mousePos = {x: 0, y: 0};
         var canvas = document.getElementById("gameCanvas");
         dl.ctx = canvas.getContext("2d");
+        canvas.addEventListener('mousemove', function(evt) {
+            dl.mousePos = that.getMousePos(canvas, evt);
+        }, false);
+    };
+    
+    _main.getMousePos = function (canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
     };
     
     _main.run = function () {
@@ -24,19 +37,29 @@
   
     _main.startLoop = function () {
         var that = this;
-        var timeFrameMiliSec = 1000 / FRAME_RATE;
+        var timeFrameMiliSec = 1000 / dl.FRAME_RATE;
 
         setInterval(function () {
          that.loopAction();
         }, timeFrameMiliSec);
-        
-        dl.ctx.addEventListener('mousemove', function(evt) {
-            dl.mousePos = getMousePos(canvas, evt);
-        }, false);
     };
     
     _main.loopAction = function () {
-        //TODO add cucco
+        //TODO pop cucco
+        //Move
+        for (var i = 0; i < this.coccu.lenght; i++) {
+            this.coccu[i].move();
+        }
+        this.link.move();
+        //Display
+        //clean screen
+        
+        //cucco
+        for (var i = 0; i < this.coccu.lenght; i++) {
+            this.coccu[i].animation();
+        }
+        //link
+        this.link.animation();        
     };
     
     window.onload = function () {
