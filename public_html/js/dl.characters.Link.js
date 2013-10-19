832adeace8557a55a92175f8fff9111df0d30351
Armand Besombes <armand.besombes@epitech.net>
2013-10-19 05:42:14 +0200

    Link Follow the cursor

--- public_html/js/dl.characters.Link.js
+++ public_html/js/dl.characters.Link.js
@@ -2,11 +2,14 @@
     
     var SIZE = 10;
     var SPEED = 10;
-    
+        
     var Link = function (x, y) {
-      dl.Character(this, x, y, SIZE);
+      //dl.Character(this, x, y, SIZE);
+      this.x = 0;
+      this.y = 0;
       this.dirX = 0;
       this.dirY = 0;
+      this.speed = SPEED;
       
       //TODO LATER
       this.loadSprite();
@@ -15,20 +18,30 @@
     dl.characters.Link = Link;
     var _link = Link.prototype;
     
-    _link.animation = function() {
+    _link.loadSprite = function() {
         //TODO LATER
     };
     
+    _link.animation = function() {
+        //DEBUG FOR NOW, SPRITE COME LATER
+        dl.ctx.fillRect(this.x, this.y, 20, 20);
+    };
+    
     _link.calcDirection = function () {
-        this.dirX = normalize(dl.mousePos.x - this.x);
-        this.dirY = normalize(dl.mousePos.y - this.y);        
+        
+        var vector =  {
+                x: dl.mousePos.x - this.x,
+                y: dl.mousePos.y - this.y
+        };
+        vector = Math.normalize(vector);
+        this.dirX = vector.x;
+        this.dirY = vector.y;
     };
     
     _link.move = function() {
         this.calcDirection();
         
-        this.x += this.dirX * 1 / dl.Main.FRAM_RATE * SPEED;
-        this.y += this.dirY * 1 / dl.Main.FRAM_RATE * SPEED;
+        this.x += this.dirX * 1 / 60 * SPEED;
+        this.y += this.dirY * 1 / 60 * SPEED;
     };
-    
 }).call(this);
