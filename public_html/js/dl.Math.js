34fe7832906d83a749dee7c2d34c2cbc2cbe72ac
Armand Besombes <armand.besombes@epitech.net>
2013-10-19 05:41:49 +0200

    Make secure Math.normalize

--- public_html/js/dl.Math.js
+++ public_html/js/dl.Math.js
@@ -1,8 +1,15 @@
 (function () {
     Math.normalize = function (point) {
-        var len = Math.sqrt(point.x * point.x + point.y * point.y)
+        
+        var len = Math.sqrt((point.x * point.x) + (point.y * point.y));
         point.x /= len;
         point.y /= len;
+        
+        if (len === 0) {
+            point.x = 0;
+            point.y = 0;
+        }
+
         return point;
     };
 }).call(this);
