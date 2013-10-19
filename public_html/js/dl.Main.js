7bbaeae9de40f8a14aab264cd86fd633558d847a
Armand Besombes <armand.besombes@epitech.net>
2013-10-19 03:05:01 +0200

    Init main function

---
+++ public_html/js/dl.Main.js
@@ -0,0 +1,41 @@
+(function () {
+    
+    var FRAME_RATE = 60;
+    
+    var Main = function() {
+        this.coccu = [];
+        this.link = null;
+        this.background = null;
+        this.hud = null;
+        this.init();
+    };
+    
+    var _main = Main.prototype;
+    
+    _main.init = function () { 
+        var canvas = document.getElementById("gameCanvas");
+        dl.ctx = canvas.getContext("2d");
+    };
+    
+    _main.run = function () {
+        this.startLoop();
+    };
+  
+    _main.startLoop = function () {
+        var that = this;
+        var timeFrameMiliSec = 1000 / FRAME_RATE;
+
+        setInterval(function () {
+         that.loopAction();
+        }, timeFrameMiliSec);
+    };
+    
+    _main.loopAction = function () {
+        
+        
+    };
+    
+    window.onload = function () {
+        new Main().run();
+    };
+}).call(this);
