74c9d0b8a5447d467335c3a69fb7c0bf60a46171
Armand Besombes <armand.besombes@epitech.net>
2013-10-23 20:08:56 +0200

    Pop Cucco from differents places

--- public_html/js/dl.characters.Cucco.js
+++ public_html/js/dl.characters.Cucco.js
@@ -1,13 +1,17 @@
 (function () {
     
     var SIZE = 10;
-    
-    var Cucco = function (x, y) {
-      dl.Character(this, x, y, SIZE);
-      //TODO taget the player;
-      var dirX = 0;
-      var dirY = 0;     
+    var SPEED = 10;
+
+    var Cucco = function () {
+      //dl.Character(this, x, y, SIZE);
+      this.dirX = 0;
+      this.dirY = 0;     
+      this.x = 0;
+      this.y = 0;
       
+      this.initPosition();
+      this.calcDirection();
       //TODO LATER
       this.loadSprite();
     };
@@ -15,13 +19,50 @@
     dl.characters.Cucco = Cucco;
     var _cucco = Cucco.prototype;
     
+    _cucco.initPosition = function () {
+      var randResult = Math.random();  
+        console.log (randResult);
+        if (randResult <= 0.25) { //TOP
+            this.x = 0;
+            this.y = Math.random() * dl.values.WIDTH;
+        } else if (randResult <= 0.5) { //BOTTOM
+            this.x = Math.random() * dl.values.HEIGHT;       
+            this.y = dl.values.HEIGHT;
+        } else if (randResult <= 0.75) { //LEFT
+            this.x = 0;
+            this.y = Math.random() * dl.values.WIDTH;;
+        } else { //RIGHT
+            this.x = Math.random() * dl.values.HEIGHT;
+            this.y = dl.values.WIDTH;
+        }
+    };
+    
     _cucco.animation = function() {
-        //TODO LATER
+        dl.ctx.fillRect(this.x, this.y, 20, 20);
+    };
+    
+    _cucco.calcDirection = function () {
+        
+        var vector =  {
+                x: dl.mainCharacter.x - this.x,
+                y: dl.mainCharacter.y - this.y
+        };
+        vector = Math.normalize(vector);
+        this.dirX = vector.x;
+        this.dirY = vector.y;
     };
     
     _cucco.move = function() {
-        this.x += this.dirX * 1 / dl.Main.FRAM_RATE;
-        this.y += this.dirY * 1 / dl.Main.FRAM_RATE;
+        this.x += this.dirX * 1 / dl.values.FRAME_RATE * SPEED;
+        this.y += this.dirY * 1 / dl.values.FRAME_RATE * SPEED;
+        
+        if (this.x < 0 || this.y < 0 || this.x > dl.values.WIDTH || this.y > dl.values.HEIGTH) {
+            this.calcDirection();
+        }
+    };
+    
+    _cucco.loadSprite = function () {
+        
     };
     
 }).call(this);
